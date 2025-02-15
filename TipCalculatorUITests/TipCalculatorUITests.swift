674abbe4b58f652f33fe84f1b2508c9542a82811
alphagamer <hemanth2k14@gmail.com>
2025-02-15 16:50:30 +0530

    added ui tests

--- TipCalculatorUITests/TipCalculatorUITests.swift
+++ TipCalculatorUITests/TipCalculatorUITests.swift
@@ -8,36 +8,49 @@
 import XCTest
 
 final class TipCalculatorUITests: XCTestCase {
+    
+    var app: XCUIApplication!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
+        app = XCUIApplication()
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app.launch()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        app = nil
+    }
+    
+    func test_textfields_placeholders() {
+        XCTAssertEqual(app.textFields["tfBillAmount"].value as? String, "Enter Bill Amount")
+        XCTAssertEqual(app.textFields["tfTipPercent"].value as? String, "Enter Tip Percentage")
+    }
+    
+    func test_tip_is_displayed() {
+        let tfBillAmount = app.textFields["tfBillAmount"]
+        let tfTipPercent = app.textFields["tfTipPercent"]
+        
+        let lblTipAmount = app.staticTexts["lblTipAmount"]
+        
+        let btnCalculateTip = app.buttons["btnCalculateTip"]
+        
+        tfBillAmount.tap()
+        tfBillAmount.typeText("100")
+        
+        tfTipPercent.tap()
+        tfTipPercent.typeText("20")
+        
+        btnCalculateTip.tap()
+        
+        let _ = lblTipAmount.waitForExistence(timeout: 1)
+        
+        XCTAssertEqual(lblTipAmount.label, "Tip Amount : $ 20.0")
     }
 
-    @MainActor
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    @MainActor
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
